b12631c95e672ae053b188d101a6421f7ce79229
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-01 15:08:58 -0600

    adds passing validation test for Invoice model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Invoice, type: :model do
     it { should belong_to :customer }
     it { should belong_to :merchant }
   end
+
+  describe "validations" do
+    it { should validate_presence_of :status }
+  end
 end
