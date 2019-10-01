55d26e333fe745efa3a48fd9a08f869ca3b3c81c
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-01 15:58:07 -0600

    successfully ran rake task for transactions data from CSV; changed datatype of credit card number to BIGINT

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_212852) do
+ActiveRecord::Schema.define(version: 2019_10_01_215450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_212852) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer "credit_card_number"
+    t.bigint "credit_card_number"
     t.string "result"
     t.string "credit_card_expiration_date"
     t.bigint "invoice_id"
