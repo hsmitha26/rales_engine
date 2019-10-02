ff385a1cac9b69b21f87216bfd06f156f3875624
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-02 15:54:42 -0600

    adds show action to Invoice resource, all tests passing

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -10,4 +10,13 @@ describe "Invoices API" do
     expect(response).to be_successful
     expect(invoices["data"].count).to eq(5)
   end
+
+  it "can get an invoice by its id" do
+    id = create(:invoice).id
+
+    get "/api/v1/invoices/#{id}"
+
+    invoice = JSON.parse(response.body)
+    expect(invoice["data"]["id"]).to eq(id.to_s)
+  end
 end
